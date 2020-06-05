cb88e5d3d64b0cee384bf8a79b5c4cd30dbc46b4
Misael Hernandez <darklink2901@gmail.com>
2020-06-05 15:16:07 -0500

    se agrego proyecto integrador

--- Proyecto 1/proyecto1/src/libreria/settings.py
+++ Proyecto 1/proyecto1/src/libreria/settings.py
@@ -15,7 +15,6 @@ from django.urls import reverse_lazy
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -130,6 +129,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
--- Proyecto 1/proyecto1/src/registro/models.py
+++ Proyecto 1/proyecto1/src/registro/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 class Registrado(models.Model):
     usuario = models.CharField(max_length = 70,unique = True)
